85c4696828dbcea0b2501177d436968ae7c755fc
diland0671 <106462939+diland0671@users.noreply.github.com>
2023-10-14 02:20:35 -0600

    Update linear_regression.py

--- content/mnist/part1/linear_regression.py
+++ content/mnist/part1/linear_regression.py
@@ -7,15 +7,19 @@ def closed_form(X, Y, lambda_factor):
     Computes the closed form solution of linear regression with L2 regularization
 
     Args:
-        X - (n, d + 1) NumPy array (n datapoints each with d features plus the bias feature in the first dimension)
-        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
-            data point
+        X - (n, d) NumPy array (n datapoints each with d features)
+        Y - (n, ) NumPy array containing the labels for each data point
         lambda_factor - the regularization constant (scalar)
     Returns:
-        theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
-        represents the y-axis intercept of the model and therefore X[0] = 1
+        theta - (d, ) NumPy array containing the weights of linear regression
     """
-    # YOUR CODE HERE
+    n, d = X.shape
+    identity_matrix = np.identity(d)
+
+    # Compute the closed-form solution
+    theta = np.linalg.inv(X.T @ X + lambda_factor * identity_matrix) @ X.T @ Y
+    
+    return theta
     raise NotImplementedError
 
 ### Functions which are already complete, for you to use ###
